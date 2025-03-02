35ca8af8c97f2cbf16a0306e13f3232ea458780c
skrased2006 <sk2006rased@gmail.com>
2025-03-02 10:34:30 +0600

    added togol

---
+++ togol.js
@@ -0,0 +1,18 @@
+document.getElementById("cashout").style.display="none";
+    document.getElementById("ad-money").style.display="block";
+
+
+
+document.getElementById("add-money-box").addEventListener("click",function(){
+    document.getElementById("cashout").style.display="none";
+    document.getElementById("ad-money").style.display="block";
+  
+})
+
+document.getElementById("cashout-box").addEventListener("click",function(){
+    document.getElementById("ad-money").style.display="none";
+    document.getElementById("cashout").style.display="block";
+   
+})
+
+
